7c64ad5fe56771d991193a1a4311caf6b7624724
feltaz <ouerfelli.moetaz@gmail.com>
2021-12-12 20:54:44 +0100

    added some stuff the controller

--- src/main/java/com/feltaz/budgetapp/loginController.java
+++ src/main/java/com/feltaz/budgetapp/loginController.java
@@ -1,18 +1,63 @@
 package com.feltaz.budgetapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
-public class loginController {
+import java.io.File;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+public class loginController implements Initializable {
     @FXML
     private Button registerButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView lockImageView;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField enterPasswordField;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        File brandingFile = new File("assets/img/leftAnchor.png");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+        File lockFile = new File("assets/img/lock.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
+    }
+
+    public void loginButtonOnAction(ActionEvent event){
+
+        if(usernameTextField.getText().isBlank()== false && enterPasswordField.getText().isBlank()==false){
+            validateLogin();
+        } else {
+            loginMessageLabel.setText("Please enter username and password");
+        }
+    }
 
     @FXML
     protected void registerButtonOnAction(ActionEvent event) {
 
         Stage stage = (Stage) registerButton.getScene().getWindow();
         stage.close();
+    }
+
+    public void validateLogin(){
+
     }
 }
