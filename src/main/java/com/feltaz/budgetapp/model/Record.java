ba629c605ff10fb7ef42457481018d26bf22b3fe
feltaz <ouerfelli.moetaz@gmail.com>
2021-12-13 08:53:48 +0100

    all the entries are viewable from the homePage

--- src/main/java/com/feltaz/budgetapp/model/Record.java
+++ src/main/java/com/feltaz/budgetapp/model/Record.java
@@ -5,70 +5,45 @@ import java.time.LocalTime;
 
 public class Record {
     int id;
-    String category;
-    Account account = new Account();
+    String account;
     double amount;
-    LocalDate date;
-    LocalTime time;
-    Currencies currency;
-
-    public void setId(int id) {
-        this.id = id;
+    String category;
+    public Record(int id,String account,double amount,String category) {
+        this.id=id;
+        this.account=account;
+        this.amount=amount;
+        this.category=category;
     }
 
-    public int getId() {
-        return id;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
-    public void setCurrency(Currencies currency) {
-        this.currency = currency;
+    public void setAccount(String account) {
+        this.account = account;
     }
 
     public void setAmount(double amount) {
         this.amount = amount;
     }
 
-    public void setTime(LocalTime time) {
-        this.time = time;
+    public int getId() {
+        return id;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    //public void setCategory(Categories category) {
-      //  this.category = category;
-    //}
-
-    public Currencies getCurrency() {
-        return currency;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public LocalDate getDate() {
-        return date;
+    public String getAccount() {
+        return account;
     }
 
     public double getAmount() {
         return amount;
     }
 
-    public Account getAccount() {
-        return account;
-    }
-
     public String getCategory() {
         return category;
-    }
-
-    public void setCategory(String value) {
-        this.category=value;
     }
 }
